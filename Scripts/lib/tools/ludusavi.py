1b9ddd8c3fdc46de90ba2e266772eb03b4e4d003
NearlyTRex <evermoresecrets@gmail.com>
2023-12-09 12:52:40 -0500

    Adding config files to ludusavi

--- Scripts/lib/tools/ludusavi.py
+++ Scripts/lib/tools/ludusavi.py
@@ -6,11 +6,66 @@ import sys
 lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(lib_folder)
 import config
+import system
 import network
 import programs
+import environment
 
 # Local imports
 from . import base
+
+# Config file
+config_files = {}
+config_files["Ludusavi/windows/ludusavi.portable"] = ""
+config_files["Ludusavi/windows/config.yaml"] = """
+---
+runtime:
+  threads: ~
+manifest:
+  url: "https://raw.githubusercontent.com/mtkennerly/ludusavi-manifest/master/data/manifest.yaml"
+language: en-US
+theme: light
+roots: []
+redirects: []
+backup:
+  path: .\\ludusavi-backup
+restore:
+  path: .\\ludusavi-backup
+scan:
+  showDeselectedGames: true
+  showUnchangedGames: true
+  showUnscannedGames: true
+cloud:
+  remote: ~
+  path: ludusavi-backup
+  synchronize: true
+customGames: []
+"""
+config_files["Ludusavi/linux/ludusavi.portable"] = ""
+config_files["Ludusavi/linux/config.yaml"] = """
+---
+runtime:
+  threads: ~
+manifest:
+  url: "https://raw.githubusercontent.com/mtkennerly/ludusavi-manifest/master/data/manifest.yaml"
+language: en-US
+theme: light
+roots: []
+redirects: []
+backup:
+  path: ./ludusavi-backup
+restore:
+  path: ./ludusavi-backup
+scan:
+  showDeselectedGames: true
+  showUnchangedGames: true
+  showUnscannedGames: true
+cloud:
+  remote: ~
+  path: ludusavi-backup
+  synchronize: true
+customGames: []
+"""
 
 # Ludusavi tool
 class Ludusavi(base.ToolBase):
@@ -58,5 +113,22 @@ class Ludusavi(base.ToolBase):
                 install_name = "Ludusavi",
                 install_dir = programs.GetProgramInstallDir("Ludusavi", "linux"),
                 install_files = ["ludusavi"],
+                chmod_files = [
+                    {
+                        "file": "ludusavi",
+                        "perms": 755
+                    }
+                ],
+                verbose = verbose,
+                exit_on_failure = exit_on_failure)
+
+    # Setup
+    def Setup(self, verbose = False, exit_on_failure = False):
+
+        # Create config files
+        for config_filename, config_contents in config_files.items():
+            system.TouchFile(
+                src = os.path.join(environment.GetToolsRootDir(), config_filename),
+                contents = config_contents,
                 verbose = verbose,
                 exit_on_failure = exit_on_failure)
