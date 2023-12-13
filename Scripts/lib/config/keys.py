10bcc001e217f74bf16581b793a57abd465e9d57
NearlyTRex <evermoresecrets@gmail.com>
2023-12-13 11:52:01 -0500

    Adding keys and types config modules

---
+++ Scripts/lib/config/keys.py
@@ -0,0 +1,11 @@
+# Imports
+import os
+import sys
+
+# Platform keys
+platform_key_supercategory = "supercategory"
+platform_key_category = "category"
+platform_key_subcategory = "subcategory"
+platform_key_transforms = "transforms"
+platform_key_addons = "addons"
+platform_key_launcher = "launcher"
---
+++ Scripts/lib/config/types.py
@@ -0,0 +1,22 @@
+# Imports
+import os
+import sys
+
+# Transform types
+transform_type_exe_to_install = "exe_to_install"
+transform_type_exe_to_raw_plain = "exe_to_raw_plain"
+transform_type_chd_to_iso = "chd_to_iso"
+transform_type_iso_to_xiso = "iso_to_xiso"
+transform_type_iso_to_raw_plain = "iso_to_raw_plain"
+transform_type_iso_to_raw_ps3 = "iso_to_raw_ps3"
+transform_type_pkg_to_raw_ps3 = "pkg_to_raw_ps3"
+transform_type_pkg_to_raw_psv = "pkg_to_raw_psv"
+
+# Addon types
+addon_type_dlc = "dlc"
+addon_type_updates = "updates"
+
+# Launch types
+launch_type_none = "no_launcher"
+launch_type_file = "launch_file"
+launch_type_name = "launch_name"
