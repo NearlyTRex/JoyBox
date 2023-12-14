577340ae49f5511c80f2551e4467c954145fdb38
NearlyTRex <evermoresecrets@gmail.com>
2023-12-13 23:41:55 -0500

    Removing 7za dependency

--- Scripts/lib/tools/sevenzip.py
+++ Scripts/lib/tools/sevenzip.py
@@ -18,19 +18,11 @@ class SevenZip(toolbase.ToolBase):
 
         # Get git info
         sevenzip_exe = ini.GetIniValue("Tools.7Zip", "7z_exe")
-        sevenzip_standalone_exe = ini.GetIniValue("Tools.7Zip", "7z_standalone_exe")
         sevenzip_install_dir = ini.GetIniValue("Tools.7Zip", "7z_install_dir")
 
         # Return config
         return {
-
-            # 7-Zip
             "7-Zip": {
                 "program": os.path.join(sevenzip_install_dir, sevenzip_exe)
-            },
-
-            # 7-Zip-Standalone
-            "7-Zip-Standalone": {
-                "program": os.path.join(sevenzip_install_dir, sevenzip_standalone_exe),
             }
         }
