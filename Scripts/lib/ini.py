16f642cb5591c1cd63b598f443f16b6a2dc6f9dc
NearlyTRex <evermoresecrets@gmail.com>
2023-12-14 00:41:04 -0500

    Adding func to initialize ini file

--- Scripts/lib/ini.py
+++ Scripts/lib/ini.py
@@ -3,12 +3,40 @@ import os, os.path
 import sys
 import configparser
 
+# Local imports
+import config
+import system
+import environment
+
 # Ini file location
 ini_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", ".."))
 ini_file = os.path.join(ini_folder, "JoyBox.ini")
 
 # Ini file parser
 ini_parser = configparser.ConfigParser()
+
+# Initialize ini file
+def InitializeIniFile(verbose = False, exit_on_failure = False):
+
+    # Ignore already existing file
+    if os.path.exists(ini_file):
+        return
+
+    # Get example file
+    new_ini_file = None
+    if environment.IsWindowsPlatform():
+        new_ini_file = os.path.join(ini_folder, "JoyBox.windows.ini.example")
+    elif environment.IsLinuxPlatform():
+        new_ini_file = os.path.join(ini_folder, "JoyBox.linux.ini.example")
+
+    # Copy file
+    if new_ini_file:
+        system.CopyFileOrDirectory(
+            src = os.path.join(ini_folder, ini_filename),
+            dest = ini_file,
+            skip_existing = True,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
 
 # Get ini sections
 def GetIniSections():
