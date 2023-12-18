0772223057b28bba305668758a202051a9a7bfc6
NearlyTRex <evermoresecrets@gmail.com>
2023-12-18 12:09:06 -0500

    Adding script to download gog game

---
+++ Scripts/bin/download_gog_game.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+import argparse
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import programs
+import command
+import ini
+import setup
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Download GOG game.")
+parser.add_argument("game_pattern", help="Game to download (regex pattern)")
+parser.add_argument("-i", "--include",
+    choices=[
+        "installers",
+        "extras"
+    ],
+    default="installers",
+    help="Files to include"
+)
+parser.add_argument("-p", "--platform",
+    choices=[
+        "windows",
+        "linux"
+    ],
+    default="windows",
+    help="Download platform"
+)
+parser.add_argument("-o", "--output_dir", type=str, default=".", help="Output directory for downloads")
+args, unknown = parser.parse_known_args()
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+    # Get flags
+    verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
+    exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
+
+    # Get download command
+    download_cmd = [
+        programs.GetToolProgram("LGOGDownloader"),
+        "--download",
+        "--platform=%s" % args.platform,
+        "--include=%s" % args.include,
+        "--directory=%s" % os.path.realpath(args.output_dir),
+        "--game=%s" % args.game_pattern
+    ]
+
+    # Run download command
+    command.RunCheckedCommand(
+        cmd = download_cmd,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+
+# Start
+main()
