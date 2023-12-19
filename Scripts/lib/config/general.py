8fea0fd985e7a4d6de5699fe027dda3c4c10951c
NearlyTRex <evermoresecrets@gmail.com>
2023-12-19 13:05:46 -0500

    Moving types to types module

--- Scripts/lib/config/general.py
+++ Scripts/lib/config/general.py
@@ -6,11 +6,6 @@ import getpass
 # Project
 project_name = "JoyBox"
 
-# Units
-unit_type_seconds = "seconds"
-unit_type_minutes = "minutes"
-unit_type_hours = "hours"
-
 # Tokens
 token_rom_storage_root = "$ROM_STORAGE_ROOT"
 token_rom_json_root = "$ROM_JSON_ROOT"
@@ -57,13 +52,6 @@ token_disc_names = {
     token_disc_six_root: "Disc 6",
     token_disc_seven_root: "Disc 7"
 }
-
-# Prefixes
-prefix_name_default = "Default"
-prefix_name_tool = "Tool"
-prefix_name_emulator = "Emulator"
-prefix_name_game = "Game"
-prefix_name_setup = "Setup"
 
 # Separators
 os_curdir = os.sep
@@ -129,70 +117,6 @@ drives_special = [
 drives_regular = []
 for code in range(ord(drive_available_start), ord(drive_available_end)):
     drives_regular.append(chr(code))
-
-# Save formats
-save_format_general = "General"
-save_format_wine = "Wine"
-save_format_sandboxie = "Sandboxie"
-
-# Capture types
-capture_type_none = "none"
-capture_type_screenshot = "screenshot"
-capture_type_video = "video"
-
-# Disc types
-disc_type_normal = "normal"
-disc_type_macwin = "macwin"
-
-# Asset types
-asset_type_background = "Background"
-asset_type_boxback = "BoxBack"
-asset_type_boxfront = "BoxFront"
-asset_type_label = "Label"
-asset_type_screenshot = "Screenshot"
-asset_type_video = "Video"
-asset_types_all = [
-    asset_type_background,
-    asset_type_boxback,
-    asset_type_boxfront,
-    asset_type_label,
-    asset_type_screenshot,
-    asset_type_video
-]
-asset_types_min = [
-    asset_type_background,
-    asset_type_boxback,
-    asset_type_boxfront,
-    asset_type_screenshot
-]
-asset_type_extensions = {
-    asset_type_background: ".jpg",
-    asset_type_boxback: ".jpg",
-    asset_type_boxfront: ".jpg",
-    asset_type_label: ".png",
-    asset_type_screenshot: ".jpg",
-    asset_type_video: ".mp4",
-}
-
-# Message types
-message_type_general = "general"
-message_type_ok = "ok"
-message_type_yes_no = "yesno"
-message_type_cancel = "cancel"
-message_type_ok_cancel = "ok_cancel"
-message_type_error = "error"
-message_type_auto_close = "auto_close"
-message_type_get_text = "get_text"
-message_type_get_file = "get_file"
-message_type_get_folder = "get_folder"
-
-# Installer formats
-installer_format_inno = "inno"
-installer_format_nsis = "nsis"
-installer_format_ins = "installshield"
-installer_format_7zip = "7zip"
-installer_format_winrar = "winrar"
-installer_format_unknown = "unknown"
 
 # Raw files index
 raw_files_index = "raw_files.index"
--- Scripts/lib/config/types.py
+++ Scripts/lib/config/types.py
@@ -33,3 +33,79 @@ addon_updates = "updates"
 launch_none = "no_launcher"
 launch_file = "launch_file"
 launch_name = "launch_name"
+
+# Units
+unit_type_seconds = "seconds"
+unit_type_minutes = "minutes"
+unit_type_hours = "hours"
+
+# Prefixes
+prefix_name_default = "Default"
+prefix_name_tool = "Tool"
+prefix_name_emulator = "Emulator"
+prefix_name_game = "Game"
+prefix_name_setup = "Setup"
+
+# Save formats
+save_format_general = "General"
+save_format_wine = "Wine"
+save_format_sandboxie = "Sandboxie"
+
+# Capture types
+capture_type_none = "none"
+capture_type_screenshot = "screenshot"
+capture_type_video = "video"
+
+# Disc types
+disc_type_normal = "normal"
+disc_type_macwin = "macwin"
+
+# Asset types
+asset_type_background = "Background"
+asset_type_boxback = "BoxBack"
+asset_type_boxfront = "BoxFront"
+asset_type_label = "Label"
+asset_type_screenshot = "Screenshot"
+asset_type_video = "Video"
+asset_types_all = [
+    asset_type_background,
+    asset_type_boxback,
+    asset_type_boxfront,
+    asset_type_label,
+    asset_type_screenshot,
+    asset_type_video
+]
+asset_types_min = [
+    asset_type_background,
+    asset_type_boxback,
+    asset_type_boxfront,
+    asset_type_screenshot
+]
+asset_type_extensions = {
+    asset_type_background: ".jpg",
+    asset_type_boxback: ".jpg",
+    asset_type_boxfront: ".jpg",
+    asset_type_label: ".png",
+    asset_type_screenshot: ".jpg",
+    asset_type_video: ".mp4",
+}
+
+# Message types
+message_type_general = "general"
+message_type_ok = "ok"
+message_type_yes_no = "yesno"
+message_type_cancel = "cancel"
+message_type_ok_cancel = "ok_cancel"
+message_type_error = "error"
+message_type_auto_close = "auto_close"
+message_type_get_text = "get_text"
+message_type_get_file = "get_file"
+message_type_get_folder = "get_folder"
+
+# Installer formats
+installer_format_inno = "inno"
+installer_format_nsis = "nsis"
+installer_format_ins = "installshield"
+installer_format_7zip = "7zip"
+installer_format_winrar = "winrar"
+installer_format_unknown = "unknown"
