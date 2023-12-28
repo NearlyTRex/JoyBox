b915e4e8ec7f0dc0b1b74cf0e479c0ed0e861894
NearlyTRex <evermoresecrets@gmail.com>
2023-12-28 13:03:49 -0500

    Updating checks

--- Scripts/bin/mount_shares.py
+++ Scripts/bin/mount_shares.py
@@ -19,23 +19,23 @@ def main():
     # Check requirements
     setup.CheckRequirements()
 
-    # Get config info
+    # Get nas info
     nas_base_location = ini.GetIniValue("UserData.NAS", "nas_base_location")
     nas_storage_folder = ini.GetIniValue("UserData.NAS", "nas_storage_folder")
     nas_cache_folder = ini.GetIniValue("UserData.NAS", "nas_cache_folder")
     nas_username = ini.GetIniValue("UserData.NAS", "nas_username")
     nas_password = ini.GetIniValue("UserData.NAS", "nas_password")
-    verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
-    exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
 
-    # Check config info
+    # Check nas info
     system.AssertIsNonEmptyString(nas_base_location, "nas_base_location")
     system.AssertIsNonEmptyString(nas_storage_folder, "nas_storage_folder")
     system.AssertIsNonEmptyString(nas_cache_folder, "nas_cache_folder")
     system.AssertIsNonEmptyString(nas_username, "nas_username")
     system.AssertIsNonEmptyString(nas_password, "nas_password")
-    system.AssertIsBool(verbose, "verbose")
-    system.AssertIsBool(exit_on_failure, "exit_on_failure")
+
+    # Get flags
+    verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
+    exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
 
     # Mount storage
     network.MountNetworkShare(
