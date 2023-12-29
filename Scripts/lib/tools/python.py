bc3a85d4e9151f9bbc7d2779d048be430b6882b0
NearlyTRex <evermoresecrets@gmail.com>
2023-12-29 14:08:29 -0500

    Adding setup for bin wrapper scripts

--- Scripts/lib/tools/python.py
+++ Scripts/lib/tools/python.py
@@ -3,8 +3,22 @@ import os, os.path
 import sys
 
 # Local imports
+import config
 import ini
+import system
+import environment
+import programs
 import toolbase
+
+# Wrapper scripts
+wrapper_script_windows = """
+@echo off
+$PYTHON_BIN "%~dp0%~n0.py" %*
+"""
+wrapper_script_unix = """
+#!/bin/bash
+exec $PYTHON_BIN "$0.py" "$@"
+"""
 
 # Python tool
 class Python(toolbase.ToolBase):
@@ -47,3 +61,39 @@ class Python(toolbase.ToolBase):
                 }
             }
         }
+
+    # Setup
+    def Setup(self, verbose = False, exit_on_failure = False):
+
+        # Create wrapper scripts
+        for obj in system.GetDirectoryContents(environment.GetScriptsBinDir()):
+            if obj.endswith(".py"):
+
+                # Get script info
+                script_basename = system.GetFilenameBasename(obj)
+                script_path_orig = os.path.join(environment.GetScriptsBinDir(), obj)
+                script_path_windows = os.path.join(environment.GetScriptsBinDir(), script_basename + ".bat")
+                script_path_unix = os.path.join(environment.GetScriptsBinDir(), script_basename)
+
+                # Create windows wrapper script
+                if environment.IsWindowsPlatform():
+                    success = system.TouchFile(
+                        src = script_path_windows,
+                        contents = wrapper_script_windows.strip().replace(config.token_python_bin, programs.GetToolProgram("PythonVenvPython")),
+                        verbose = verbose,
+                        exit_on_failure = exit_on_failure)
+                    system.AssertCondition(success, "Could not setup Python wrapper scripts")
+
+                # Create unix wrapper script
+                if environment.IsUnixPlatform():
+                    success = system.TouchFile(
+                        src = script_path_unix,
+                        contents = wrapper_script_unix.strip().replace(config.token_python_bin, programs.GetToolProgram("PythonVenvPython")),
+                        verbose = verbose,
+                        exit_on_failure = exit_on_failure)
+                    system.AssertCondition(success, "Could not setup Python wrapper scripts")
+                    success = system.MarkAsExecutable(
+                        src = script_path_unix,
+                        verbose = verbose,
+                        exit_on_failure = exit_on_failure)
+                    system.AssertCondition(success, "Could not setup Python wrapper scripts")
