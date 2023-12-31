e77fa1014a6126c707d0a4b24264d03bcaf5497e
NearlyTRex <evermoresecrets@gmail.com>
2023-12-31 17:57:11 -0500

    Merging selection of random entry into launch_json Allowing to specify json categories instead of a json file

--- Scripts/bin/launch_json.py
+++ Scripts/bin/launch_json.py
@@ -4,6 +4,7 @@
 import os, os.path
 import sys
 import argparse
+import random
 
 # Custom imports
 lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
@@ -11,7 +12,9 @@ sys.path.append(lib_folder)
 import config
 import system
 import environment
+import platforms
 import launcher
+import metadata
 import cache
 import setup
 import gameinfo
@@ -19,15 +22,16 @@ import gui
 import ini
 
 # Setup argument parser
-parser = argparse.ArgumentParser(description="Launch json file.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("path", type=str, help="Json path to launch")
+parser = argparse.ArgumentParser(description="Launch json game.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-i", "--input_file", type=str, help="Json file to launch")
+parser.add_argument("-c", "--category", type=str, help="Json category")
+parser.add_argument("-s", "--subcategory", type=str, help="Json subcategory")
+parser.add_argument("-n", "--name", type=str, help="Json name")
+parser.add_argument("-r", "--fill_with_random", action="store_true", help="Fill unspecified fields with random values")
 parser.add_argument("-f", "--force_cache_refresh", action="store_true", help="Force refresh of cached files")
 
 # Parse arguments
 args, unknownargs = parser.parse_known_args()
-if not args.path:
-    parser.print_help()
-    sys.exit(1)
 
 # Main
 def main():
@@ -43,11 +47,62 @@ def main():
     # Get capture type
     capture_type = ini.GetIniValue("UserData.Capture", "capture_type")
 
-    # Get json file
-    json_file = args.path
-    if not os.path.isfile(json_file):
-        json_file = os.path.join(environment.GetJsonRomsMetadataRootDir(), json_file)
-    if not os.path.isfile(json_file):
+    # Json file to load
+    json_file = None
+
+    # Prefer input file if it was specified
+    if args.input_file:
+        if os.path.isfile(args.input_file):
+            json_file = args.input_file
+        else:
+            json_file = os.path.join(environment.GetJsonRomsMetadataRootDir(), json_file)
+
+    # Next use category values
+    elif args.category and args.subcategory and args.name:
+        json_file = environment.GetJsonRomMetadataFile(
+            game_category = args.category,
+            game_subcategory = args.subcategory,
+            game_name = args.name)
+
+    # Finally, use random selection
+    elif args.fill_with_random:
+
+        # Get category
+        game_category = args.category
+        if not game_category:
+            game_category = random.choice(config.game_categories)
+
+        # Get subcategory
+        game_subcategory = args.subcategory
+        if not game_subcategory:
+            potential_subcategories = []
+            for potential_subcategory in config.game_subcategories[game_category]:
+                potential_platform = gameinfo.DeriveGamePlatformFromCategories(game_category, potential_subcategory)
+                if not platforms.HasNoLauncher(potential_platform):
+                    potential_subcategories.append(potential_subcategory)
+            game_subcategory = random.choice(potential_subcategories)
+
+        # Read metadata for this category/subcategory pair
+        metadata_file = environment.GetMetadataFile(game_category, game_subcategory)
+        metadata_obj = metadata.Metadata()
+        metadata_obj.import_from_metadata_file(metadata_file)
+
+        # Select random game entry
+        random_game_entry = metadata_obj.get_random_entry()
+
+        # Get json file
+        if random_game_entry:
+            json_file = environment.GetJsonRomMetadataFile(
+                game_category = random_game_entry[config.metadata_key_category],
+                game_subcategory = random_game_entry[config.metadata_key_subcategory],
+                game_name = random_game_entry[config.metadata_key_game])
+
+    # Check json file
+    if not json_file:
+        gui.DisplayErrorPopup(
+            title_text = "No json file specified",
+            message_text = "No json file was specified")
+    if json_file or not os.path.isfile(json_file):
         gui.DisplayErrorPopup(
             title_text = "Json file not found",
             message_text = "Json file %s was not found" % json_file)
