129ec070743769ff6f9abc985fdac1fb6a19f111
NearlyTRex <evermoresecrets@gmail.com>
2024-01-06 17:09:40 -0500

    Adding sync tool/module

---
+++ Scripts/bin/sync_tool.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+import argparse
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import environment
+import system
+import sync
+import setup
+import ini
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Sync tool.")
+parser.add_argument("-t", "--type",
+    choices=[
+        "gdrive"
+    ],
+    default="gdrive", help="Sync type"
+)
+parser.add_argument("-a", "--action",
+    choices=[
+        "init",
+        "download",
+        "upload",
+        "pull",
+        "push"
+    ],
+    default="init", help="Sync action"
+)
+parser.add_argument("-l", "--local_path", type=str, default=environment.GetSyncRootDir(), help="Local path")
+parser.add_argument("-r", "--remote_path", type=str, default="/", help="Remote path")
+args, unknown = parser.parse_known_args()
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+    # Get flags
+    verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
+    exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
+
+    # Init sync
+    if args.action == "init":
+        if args.type == "gdrive":
+            sync.SetupGoogleDriveRemote(
+                remote_name = args.type,
+                verbose = verbose,
+                exit_on_failure = exit_on_failure)
+
+    # Download files
+    elif args.action == "download":
+        sync.DownloadFilesFromRemote(
+            local_path = args.local_path,
+            remote_name = args.type,
+            remote_path = args.remote_path,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+
+    # Upload files
+    elif args.action == "upload":
+        sync.UploadFilesToRemote(
+            local_path = args.local_path,
+            remote_name = args.type,
+            remote_path = args.remote_path,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+
+    # Pull files
+    elif args.action == "pull":
+        sync.SyncFilesFromRemote(
+            local_path = args.local_path,
+            remote_name = args.type,
+            remote_path = args.remote_path,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+
+    # Push files
+    elif args.action == "push":
+        sync.SyncFilesToRemote(
+            local_path = args.local_path,
+            remote_name = args.type,
+            remote_path = args.remote_path,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+
+# Start
+main()
---
+++ Scripts/lib/sync.py
@@ -0,0 +1,185 @@
+# Imports
+import os
+import os.path
+import sys
+
+# Local imports
+import config
+import command
+import programs
+import system
+import environment
+
+# Setup Google Drive remote
+def SetupGoogleDriveRemote(remote_name, verbose = False, exit_on_failure = False):
+
+    # Get tool
+    rclone_tool = None
+    if programs.IsToolInstalled("RClone"):
+        rclone_tool = programs.GetToolProgram("RClone")
+    if not rclone_tool:
+        system.LogError("RClone was not found")
+        return False
+
+    # Get create command
+    create_cmd = [
+        rclone_tool,
+        "config",
+        "create", remote_name,
+        "drive",
+        "config_is_local=false"
+    ]
+    if verbose:
+        create_cmd += ["--verbose"]
+
+    # Run create command
+    code = command.RunBlockingCommand(
+        cmd = create_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    if code != 0:
+        return False
+
+    # Get authorize command
+    authorize_cmd = [
+        rclone_tool,
+        "config",
+        "reconnect", "%s:" % remote_name
+    ]
+    if verbose:
+        authorize_cmd += ["--verbose"]
+
+    # Run authorize command
+    code = command.RunBlockingCommand(
+        cmd = authorize_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return code == 0
+
+# Download files from remote
+def DownloadFilesFromRemote(local_path, remote_name, remote_path, verbose = False, exit_on_failure = False):
+
+    # Get tool
+    rclone_tool = None
+    if programs.IsToolInstalled("RClone"):
+        rclone_tool = programs.GetToolProgram("RClone")
+    if not rclone_tool:
+        system.LogError("RClone was not found")
+        return False
+
+    # Get copy command
+    copy_cmd = [
+        rclone_tool,
+        "copy",
+        "%s:%s" % (remote_name, remote_path),
+        local_path,
+        "--create-empty-src-dirs"
+    ]
+    if verbose:
+        copy_cmd += ["--verbose"]
+
+    # Run copy command
+    code = command.RunBlockingCommand(
+        cmd = copy_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return code == 0
+
+# Upload files to remote
+def UploadFilesToRemote(local_path, remote_name, remote_path, verbose = False, exit_on_failure = False):
+
+    # Get tool
+    rclone_tool = None
+    if programs.IsToolInstalled("RClone"):
+        rclone_tool = programs.GetToolProgram("RClone")
+    if not rclone_tool:
+        system.LogError("RClone was not found")
+        return False
+
+    # Get copy command
+    copy_cmd = [
+        rclone_tool,
+        "copy",
+        local_path,
+        "%s:%s" % (remote_name, remote_path),
+        "--create-empty-src-dirs"
+    ]
+    if verbose:
+        copy_cmd += ["--verbose"]
+
+    # Run copy command
+    code = command.RunBlockingCommand(
+        cmd = copy_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return code == 0
+
+# Sync files from remote
+def SyncFilesFromRemote(local_path, remote_name, remote_path, verbose = False, exit_on_failure = False):
+
+    # Get tool
+    rclone_tool = None
+    if programs.IsToolInstalled("RClone"):
+        rclone_tool = programs.GetToolProgram("RClone")
+    if not rclone_tool:
+        system.LogError("RClone was not found")
+        return False
+
+    # Get sync command
+    sync_cmd = [
+        rclone_tool,
+        "sync",
+        "%s:%s" % (remote_name, remote_path),
+        local_path,
+        "--create-empty-src-dirs"
+    ]
+    if verbose:
+        sync_cmd += ["--verbose"]
+
+    # Run sync command
+    code = command.RunBlockingCommand(
+        cmd = sync_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return code == 0
+
+# Sync files to remote
+def SyncFilesToRemote(local_path, remote_name, remote_path, verbose = False, exit_on_failure = False):
+
+    # Get tool
+    rclone_tool = None
+    if programs.IsToolInstalled("RClone"):
+        rclone_tool = programs.GetToolProgram("RClone")
+    if not rclone_tool:
+        system.LogError("RClone was not found")
+        return False
+
+    # Get sync command
+    sync_cmd = [
+        rclone_tool,
+        "sync",
+        local_path,
+        "%s:%s" % (remote_name, remote_path),
+        "--create-empty-src-dirs"
+    ]
+    if verbose:
+        sync_cmd += ["--verbose"]
+
+    # Run sync command
+    code = command.RunBlockingCommand(
+        cmd = sync_cmd,
+        options = command.CommandOptions(
+            blocking_processes = [rclone_tool]),
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return code == 0
