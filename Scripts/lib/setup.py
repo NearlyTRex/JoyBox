ff822f0958a22fec3ba92920aa65de7235bfe5ed
NearlyTRex <evermoresecrets@gmail.com>
2024-01-08 12:22:10 -0500

    Making each setup a separate func

--- Scripts/lib/setup.py
+++ Scripts/lib/setup.py
@@ -35,23 +35,25 @@ def CheckRequirements():
         system.LogError("Ini file not found, please run setup first")
         sys.exit(1)
 
-# Setup environment
-def SetupEnvironment(verbose = False, exit_on_failure = False):
-
-    # Setup tools
+# Setup tools
+def SetupTools(verbose = False, exit_on_failure = False):
     for tool in programs.GetTools():
         system.LogInfo("Installing tool %s ..." % tool.GetName())
         tool.Setup(
             verbose = verbose,
             exit_on_failure = exit_on_failure)
 
-    # Setup emulators
+# Setup emulators
+def SetupEmulators(verbose = False, exit_on_failure = False):
     for emulator in programs.GetEmulators():
         system.LogInfo("Installing emulator %s ..." % emulator.GetName())
         emulator.Setup(
             verbose = verbose,
             exit_on_failure = exit_on_failure)
 
+# Setup assets
+def SetupAssets(verbose = False, exit_on_failure = False):
+    
     # Create asset symlinks
     for game_category in config.game_categories:
         for game_subcategory in config.game_subcategories[game_category]:
