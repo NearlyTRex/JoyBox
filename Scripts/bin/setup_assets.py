ee6f6b0902c3f2104d8cc6708f010cdf1b0fad77
NearlyTRex <evermoresecrets@gmail.com>
2024-01-08 12:22:58 -0500

    Adding separate setup scripts

---
+++ Scripts/bin/setup_assets.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import setup
+import ini
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+    # Get flags
+    verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
+    exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
+
+    # Setup assets
+    setup.SetupAssets(verbose = verbose, exit_on_failure = exit_on_failure)
+
+# Start
+main()
