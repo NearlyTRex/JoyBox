1ce64e0aed3c43f498ac769247102d07b6e40022
NearlyTRex <evermoresecrets@gmail.com>
2024-01-09 19:53:19 -0500

    Updating dat parsing, adding cache file format

--- Scripts/bin/dat_renamer.py
+++ Scripts/bin/dat_renamer.py
@@ -18,6 +18,8 @@ import ini
 parser = argparse.ArgumentParser(description="Dat renamer.")
 parser.add_argument("input_path", help="Input path")
 parser.add_argument("-d", "--dat_directory", type=str, help="Dat directory")
+parser.add_argument("-c", "--dat_cachefile", type=str, help="Dat cachefile")
+parser.add_argument("-g", "--generate_cachefile", action="store_true", help="Generate collected cachefile (if scanning normal dats)")
 args, unknown = parser.parse_known_args()
 if not args.input_path:
     parser.print_help()
@@ -25,7 +27,7 @@ if not args.input_path:
 
 # Check that input path exists first
 input_path = os.path.realpath(args.input_path)
-if not os.path.exists(input_path):
+if not os.path.isdir(input_path):
     system.LogError("Path '%s' does not exist" % args.input_path)
     sys.exit(-1)
 
@@ -39,13 +41,29 @@ def main():
     verbose = ini.GetIniBoolValue("UserData.Flags", "verbose")
     exit_on_failure = ini.GetIniBoolValue("UserData.Flags", "exit_on_failure")
 
-    # Create game dat
+    # Load game dat(s)
     game_dat = dat.Dat()
     if os.path.isdir(args.dat_directory):
-        game_dat.import_clrmamepro_dat_files(args.dat_directory, verbose = verbose, exit_on_failure = exit_on_failure)
+        game_dat.import_clrmamepro_dat_files(
+            dat_dir = args.dat_directory,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+        if args.generate_cachefile:
+            game_dat.export_cache_dat_file(
+                dat_file = args.dat_cachefile,
+                verbose = verbose,
+                exit_on_failure = exit_on_failure)
+    elif os.path.isfile(args.dat_cachefile):
+        game_dat.import_cache_dat_file(
+            dat_file = args.dat_cachefile,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
 
     # Rename files
-    game_dat.rename_files(input_path, verbose = verbose, exit_on_failure = exit_on_failure)
+    game_dat.rename_files(
+        input_dir = input_path,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
 
 # Start
 main()
--- Scripts/lib/dat.py
+++ Scripts/lib/dat.py
@@ -31,12 +31,60 @@ class Dat:
             return None
         return self.game_database[md5]
 
-    # Import clrmamepro dat file
-    def import_clrmamepro_dat_file(self, input_file, verbose = False, exit_on_failure = False):
+    # Import cache dat file
+    def import_cache_dat_file(self, dat_file, verbose = False, exit_on_failure = False):
         try:
             if verbose:
-                system.Log("Importing clrmamepro dat file '%s'" % input_file)
-            dom = minidom.parse(input_file)
+                system.Log("Importing cache dat file '%s'" % dat_file)
+            with open(dat_file, "r", encoding="utf8", newline="\n") as file:
+                for line in file.readlines():
+                    line_tokens = line.strip().split(" || ")
+                    if len(line_tokens) >= 5:
+                        game_entry = {}
+                        game_entry[config.dat_key_game] = line_tokens[0]
+                        game_entry[config.dat_key_file] = line_tokens[1]
+                        game_entry[config.dat_key_size] = line_tokens[2]
+                        game_entry[config.dat_key_crc] = line_tokens[3]
+                        game_entry[config.dat_key_md5] = line_tokens[4]
+                        self.add_game(game_entry)
+            return True
+        except Exception as e:
+            if exit_on_failure:
+                system.LogError("Unable import cache dat file '%s'" % dat_file)
+                system.LogError(e)
+                sys.exit(1)
+            return False
+
+    # Export cache dat file
+    def export_cache_dat_file(self, dat_file, verbose = False, exit_on_failure = False):
+        try:
+            if verbose:
+                system.Log("Exporting cache dat file '%s'" % dat_file)
+            with open(dat_file, "w", encoding="utf8", newline="\n") as file:
+                for key in self.game_database.keys():
+                    game_entry = self.game_database[key]
+                    replacements = (
+                        game_entry[config.dat_key_game],
+                        game_entry[config.dat_key_file],
+                        game_entry[config.dat_key_size],
+                        game_entry[config.dat_key_crc],
+                        game_entry[config.dat_key_md5]
+                    )
+                    file.write("%s || %s || %s || %s || %s\n" % replacements)
+            return True
+        except Exception as e:
+            if exit_on_failure:
+                system.LogError("Unable to export cache dat file '%s'" % dat_file)
+                system.LogError(e)
+                sys.exit(1)
+            return False
+
+    # Import clrmamepro dat file
+    def import_clrmamepro_dat_file(self, dat_file, verbose = False, exit_on_failure = False):
+        try:
+            if verbose:
+                system.Log("Importing clrmamepro dat file '%s'" % dat_file)
+            dom = minidom.parse(dat_file)
             game_tags = dom.getElementsByTagName("game")
             for game_tag in game_tags:
                 rom_tags = game_tag.getElementsByTagName("rom")
@@ -50,34 +98,35 @@ class Dat:
                         continue
                     game_entry = {}
                     game_entry[config.dat_key_game] = game_tag.attributes["name"].value
-                    game_entry[config.dat_key_file] = rom_tag.attributes["name"].value
+                    game_entry[config.dat_key_file] = system.GetFilenameFile(rom_tag.attributes["name"].value.replace("\\", "/"))
                     game_entry[config.dat_key_size] = rom_tag.attributes["size"].value
                     game_entry[config.dat_key_crc] = rom_tag.attributes["crc"].value
                     game_entry[config.dat_key_md5] = rom_tag.attributes["md5"].value
                     self.add_game(game_entry)
+            return True
         except Exception as e:
             if exit_on_failure:
-                system.LogError("Unable to import clrmamepro dat file '%s'" % input_file)
+                system.LogError("Unable to import clrmamepro dat file '%s'" % dat_file)
                 system.LogError(e)
                 sys.exit(1)
             return False
 
     # Import clrmamepro dat files
-    def import_clrmamepro_dat_files(self, input_path, verbose = False, exit_on_failure = False):
-        for input_file in system.BuildFileListByExtensions(input_path, extensions = [".dat"]):
-            self.import_clrmamepro_dat_file(input_file, verbose = verbose, exit_on_failure = exit_on_failure)
+    def import_clrmamepro_dat_files(self, dat_dir, verbose = False, exit_on_failure = False):
+        for dat_file in system.BuildFileListByExtensions(dat_dir, extensions = [".dat"]):
+            self.import_clrmamepro_dat_file(dat_file, verbose = verbose, exit_on_failure = exit_on_failure)
 
     # Rename files
-    def rename_files(self, input_path, verbose = False, exit_on_failure = False):
+    def rename_files(self, input_dir, verbose = False, exit_on_failure = False):
         if verbose:
-            system.LogInfo("Renaming files in '%s' according to imported dats ..." % input_path)
-        for file in system.BuildFileList(input_path):
+            system.LogInfo("Renaming files in '%s' according to imported dats ..." % input_dir)
+        for file in system.BuildFileList(input_dir):
             if verbose:
                 system.Log("Examining '%s'" % file)
             file_dir = system.GetFilenameDirectory(file)
-            file_md5 = hashimg.CalculateFileMD5(filename, verbose = verbose, exit_on_failure = exit_on_failure)
-            if self.is_md5_present(md5):
-                game_entry = self.get_by_md5(md5)
+            file_md5 = hashing.CalculateFileMD5(file, verbose = verbose, exit_on_failure = exit_on_failure)
+            if self.is_md5_present(file_md5):
+                game_entry = self.get_by_md5(file_md5)
                 file_path_new = os.path.join(file_dir, game_entry[config.dat_key_file])
                 if file != file_path_new:
                     system.MoveFileOrDirectory(
