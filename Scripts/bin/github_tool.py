096c0bdc62c536d101f4bc2f8b37e6a0b6e0ef0a
NearlyTRex <evermoresecrets@gmail.com>
2024-01-14 18:54:03 -0500

    Adding clean support Switching recursion default

--- Scripts/bin/github_tool.py
+++ Scripts/bin/github_tool.py
@@ -28,7 +28,8 @@ parser.add_argument("-n", "--github_repository", type=str, help="Github reposito
 parser.add_argument("-t", "--github_access_token", type=str, help="Github access token")
 parser.add_argument("-d", "--archive_dir", type=str, default=environment.GetSyncedDevelopmentArchiveDir(), help="Archive directory")
 parser.add_argument("-f", "--force", action="store_true", help="Force action")
-parser.add_argument("-c", "--avoid_recursion", action="store_true", help="Avoid recursion")
+parser.add_argument("-r", "--recursive", action="store_true", help="Use recursion")
+parser.add_argument("-c", "--clean_first", action="store_true", help="Use cleaning first")
 args, unknown = parser.parse_known_args()
 
 # Check that archive dir exists first
@@ -87,11 +88,6 @@ def main():
                 else:
                     should_archive = True
 
-            # Use recursion unless told not to
-            use_recursion = True
-            if args.avoid_recursion:
-                use_recursion = False
-
             # Archive repo
             if should_archive:
                 network.ArchiveGithubRepository(
@@ -99,7 +95,8 @@ def main():
                     github_repo = github_repository,
                     github_token = github_access_token,
                     output_file = os.path.join(archive_dir, github_username, github_repository + "_" + str(environment.GetCurrentTimestamp()) + ".zip"),
-                    recursive = use_recursion,
+                    recursive = args.recursive,
+                    clean_first = args.clean_first,
                     verbose = verbose,
                     exit_on_failure = exit_on_failure)
 
