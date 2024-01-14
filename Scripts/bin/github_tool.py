32e5af32ab773f61c61ac41cc341adad93568986
NearlyTRex <evermoresecrets@gmail.com>
2024-01-14 14:22:04 -0500

    Updating github tool to allow force actions and avoiding recursion

--- Scripts/bin/github_tool.py
+++ Scripts/bin/github_tool.py
@@ -27,6 +27,8 @@ parser.add_argument("-u", "--github_username", type=str, help="Github username")
 parser.add_argument("-n", "--github_repository", type=str, help="Github repository")
 parser.add_argument("-t", "--github_access_token", type=str, help="Github access token")
 parser.add_argument("-d", "--archive_dir", type=str, default=environment.GetSyncedDevelopmentArchiveDir(), help="Archive directory")
+parser.add_argument("-f", "--force", action="store_true", help="Force action")
+parser.add_argument("-c", "--avoid_recursion", action="store_true", help="Avoid recursion")
 args, unknown = parser.parse_known_args()
 
 # Check that archive dir exists first
@@ -62,22 +64,44 @@ def main():
     if args.github_repository:
         github_repositories = [args.github_repository]
     if len(github_repositories) == 0:
-        github_repositories = network.GetGithubRepositories(
+        github_repos = network.GetGithubRepositories(
             github_user = github_username,
             github_token = github_access_token,
             verbose = verbose,
             exit_on_failure = exit_on_failure)
+        for github_repo in github_repos:
+            github_repositories.append(github_repo.name)
 
     # Archive repositories
     if args.action == "archive":
         for github_repository in github_repositories:
-            network.ArchiveGithubRepository(
-                github_user = github_username,
-                github_repo = github_repository.name,
-                github_token = github_access_token,
-                output_file = os.path.join(archive_dir, github_repository.name + "_" + str(environment.GetCurrentTimestamp()) + ".zip"),
-                verbose = verbose,
-                exit_on_failure = exit_on_failure)
+
+            # Check whether to archive
+            should_archive = False
+            if args.force:
+                should_archive = True
+            else:
+                for obj in system.GetDirectoryContents(os.path.join(archive_dir, github_username)):
+                    if obj.startswith(github_repository) and obj.endswith(".zip"):
+                        break
+                else:
+                    should_archive = True
+
+            # Use recursion unless told not to
+            use_recursion = True
+            if args.avoid_recursion:
+                use_recursion = False
+
+            # Archive repo
+            if should_archive:
+                network.ArchiveGithubRepository(
+                    github_user = github_username,
+                    github_repo = github_repository,
+                    github_token = github_access_token,
+                    output_file = os.path.join(archive_dir, github_username, github_repository + "_" + str(environment.GetCurrentTimestamp()) + ".zip"),
+                    recursive = use_recursion,
+                    verbose = verbose,
+                    exit_on_failure = exit_on_failure)
 
 # Start
 main()
