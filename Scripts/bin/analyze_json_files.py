3aa3e51c9e6e4c4bcb4c2545ed7c6bbc25967c99
NearlyTRex <evermoresecrets@gmail.com>
2024-01-25 11:00:27 -0500

    Adding start of analyze_json_files

---
+++ Scripts/bin/analyze_json_files.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+import argparse
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import config
+import environment
+import gameinfo
+import system
+import setup
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Analyze json files.")
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
+args, unknown = parser.parse_known_args()
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+    # Clean json files
+    for game_category in config.game_categories:
+        for game_subcategory in config.game_subcategories[game_category]:
+            game_platform = gameinfo.DeriveGamePlatformFromCategories(game_category, game_subcategory)
+            for game_name in gameinfo.FindAllGameNames(environment.GetJsonRomsMetadataRootDir(), game_category, game_subcategory):
+
+                # Get json file
+                json_file = environment.GetJsonRomMetadataFile(game_category, game_subcategory, game_name)
+
+                # Get game info
+                game_info = gameinfo.GameInfo(
+                    json_file = json_file,
+                    verbose = args.verbose,
+                    exit_on_failure = args.exit_on_failure)
+
+# Start
+main()
