4eb097158754bf23b8ca56715c16bfe1f10667da
NearlyTRex <evermoresecrets@gmail.com>
2024-02-21 11:31:53 -0500

    Adding download script for general youtube downloading

---
+++ Scripts/bin/download_youtube_videos.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+import argparse
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import config
+import system
+import metadata
+import setup
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Download youtube videos.")
+parser.add_argument("youtube_url", help="YouTube url")
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
+args, unknown = parser.parse_known_args()
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+# Start
+main()
