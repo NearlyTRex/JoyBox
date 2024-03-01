2eb84e0b9d14e5618caa761f877e7e5248a712e5
NearlyTRex <evermoresecrets@gmail.com>
2024-03-01 13:55:42 -0500

    Updating gog tool

--- Scripts/bin/download_gog_game.py
+++ Scripts/bin/download_gog_game.py
@@ -8,20 +8,14 @@ import argparse
 # Custom imports
 lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
 sys.path.append(lib_folder)
+import config
 import gog
+import system
 import setup
 
 # Parse arguments
-parser = argparse.ArgumentParser(description="Download GOG game.")
-parser.add_argument("game", help="Game to download (regex pattern)")
-parser.add_argument("-i", "--include",
-    choices=[
-        "installers",
-        "extras"
-    ],
-    default="installers",
-    help="Files to include"
-)
+parser = argparse.ArgumentParser(description="Download GOG games.")
+parser.add_argument("input_path", type=str, default=".", help="Input path")
 parser.add_argument("-p", "--platform",
     choices=[
         "windows",
@@ -30,15 +24,16 @@ parser.add_argument("-p", "--platform",
     default="windows",
     help="Download platform"
 )
-parser.add_argument("-o", "--output_dir", type=str, default=".", help="Output directory for downloads")
+parser.add_argument("-f", "--force_download", action="store_true", help="Always download")
+parser.add_argument("-o", "--output_dir", type=str, help="Output directory")
 parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
 parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
 args, unknown = parser.parse_known_args()
 
-# Check that output_dir exists first
-output_dir = os.path.realpath(args.output_dir)
-if not os.path.exists(output_dir):
-    system.LogError("Path '%s' does not exist" % args.output_dir)
+# Get input path
+input_path = os.path.realpath(args.input_path)
+if not os.path.exists(input_path):
+    system.LogError("Path '%s' does not exist" % args.input_path)
     sys.exit(-1)
 
 # Main
@@ -47,14 +42,15 @@ def main():
     # Check requirements
     setup.CheckRequirements()
 
-    # Download game
-    gog.DownloadGame(
-        game = args.game,
-        output_dir = output_dir,
-        platform = args.platform,
-        include = args.include,
-        verbose = args.verbose,
-        exit_on_failure = args.exit_on_failure)
+    # Download games
+    for json_file in system.BuildFileListByExtensions(input_path, extensions = [".json"]):
+        gog.DownloadGameByJsonFile(
+            json_file = json_file,
+            platform = args.platform,
+            output_dir = args.output_dir,
+            force_download = args.force_download,
+            verbose = args.verbose,
+            exit_on_failure = args.exit_on_failure)
 
 # Start
 main()
--- Scripts/lib/gog.py
+++ Scripts/lib/gog.py
@@ -3,12 +3,25 @@ import os, os.path
 import sys
 
 # Local imports
+import config
 import command
+import archive
 import programs
+import network
+import gameinfo
 import system
+import environment
 
 # Download game
-def DownloadGame(game, output_dir, platform, include, verbose = False, exit_on_failure = False):
+def DownloadGameByName(
+    appname,
+    output_dir,
+    platform,
+    include = "",
+    exclude = "",
+    clean_output_first = False,
+    verbose = False,
+    exit_on_failure = False):
 
     # Get tool
     gog_tool = None
@@ -18,15 +31,32 @@ def DownloadGame(game, output_dir, platform, include, verbose = False, exit_on_f
         system.LogError("LGOGDownloader was not found")
         sys.exit(1)
 
+    # Create temporary directory
+    tmp_dir_success, tmp_dir_result = system.CreateTemporaryDirectory(verbose = verbose)
+    if not tmp_dir_success:
+        return False
+
     # Get download command
     download_cmd = [
         gog_tool,
         "--download",
+        "--game=%s" % appname,
         "--platform=%s" % platform,
-        "--include=%s" % include,
-        "--directory=%s" % os.path.realpath(output_dir),
-        "--game=%s" % game
+        "--directory=%s" % tmp_dir_result,
+        "--check-free-space",
+        "--threads=1",
+        "--subdir-game=.",
+        "--subdir-extras=extra",
+        "--subdir-dlc=dlc"
     ]
+    if isinstance(include, str) and len(include):
+        download_cmd += [
+            "--include=%s" % include
+        ]
+    if isinstance(exclude, str) and len(exclude):
+        download_cmd += [
+            "--exclude=%s" % exclude
+        ]
 
     # Run download command
     code = command.RunBlockingCommand(
@@ -35,4 +65,137 @@ def DownloadGame(game, output_dir, platform, include, verbose = False, exit_on_f
             blocking_processes = [gog_tool]),
         verbose = verbose,
         exit_on_failure = exit_on_failure)
-    return (code == 0)
+    if (code != 0):
+        system.LogError("Files were not downloaded successfully")
+        return False
+
+    # Clean output first
+    if clean_output_first:
+        system.RemoveDirectoryContents(
+            dir = output_dir,
+            verbose = verbose,
+            exit_on_failure = exit_on_failure)
+
+    # Move downloaded files
+    success = system.MoveContents(
+        src = tmp_dir_result,
+        dest = output_dir,
+        show_progress = True,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    if not success:
+        system.RemoveDirectory(tmp_dir_result, verbose = verbose)
+        return False
+
+    # Delete temporary directory
+    system.RemoveDirectory(tmp_dir_result, verbose = verbose)
+
+    # Check result
+    return os.path.exists(output_dir)
+
+# Download game by json file
+def DownloadGameByJsonFile(
+    json_file,
+    platform,
+    output_dir = None,
+    force_download = False,
+    verbose = False,
+    exit_on_failure = False):
+
+    # Get game info
+    game_info = gameinfo.GameInfo(
+        json_file = json_file,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+
+    # Ignore non-gog games
+    if game_info.get_gog_appid() == "":
+        return False
+
+    # Get latest gog info
+    latest_gog_info = GetGameInfo(
+        appid = game_info.get_gog_appid(),
+        platform = platform,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+
+    # Get build ids
+    old_buildid = game_info.get_gog_buildid()
+    new_buildid = latest_gog_info[config.json_key_gog_buildid]
+
+    # Check if game should be downloaded
+    should_download = False
+    if force_download:
+        should_download = True
+    elif len(old_buildid) == 0:
+        should_download = True
+    elif len(old_buildid) > 0 and len(new_buildid) == 0:
+        should_download = True
+    else:
+        should_download = new_buildid != old_buildid
+    if not should_download:
+        return False
+
+    # Get output dir
+    if output_dir:
+        output_dir = os.path.realpath(output_dir)
+    else:
+        output_dir = environment.GetRomDir(game_info.get_category(), game_info.get_subcategory(), game_info.get_name())
+
+    # Download game
+    success = DownloadGameByName(
+        appname = game_info.get_gog_appname(),
+        output_dir = output_dir,
+        platform = platform,
+        include = "i,e",
+        clean_output_first = True,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    if not success:
+        return False
+
+    # Update json file
+    json_data = system.ReadJsonFile(
+        src = json_file,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    json_data[config.json_key_gog] = latest_gog_info
+    success = system.WriteJsonFile(
+        src = json_file,
+        json_data = json_data,
+        sort_keys = True,
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    return success
+
+# Get game info
+def GetGameInfo(appid, platform, verbose = False, exit_on_failure = False):
+
+    # Get gog url
+    gog_url = "https://api.gog.com/products/%s?expand=downloads" % appid
+
+    # Get gog json
+    gog_json = network.GetRemoteJson(
+        url = gog_url,
+        headers = {"Accept": "application/json"},
+        verbose = verbose,
+        exit_on_failure = exit_on_failure)
+    if not gog_json:
+        system.LogError("Unable to find gog release information from '%s'" % gog_url)
+        return False
+
+    # Build game info
+    game_info = {}
+    game_info[config.json_key_gog_appid] = appid
+    if "slug" in gog_json:
+        game_info[config.json_key_gog_appname] = gog_json["slug"]
+    if "title" in gog_json:
+        game_info[config.json_key_gog_name] = gog_json["title"].strip()
+    if "downloads" in gog_json:
+        appdownloads = gog_json["downloads"]
+        if "installers" in appdownloads:
+            appinstallers = appdownloads["installers"]
+            for appinstaller in appinstallers:
+                if appinstaller["os"] == platform:
+                    game_info[config.json_key_gog_buildid] = appinstaller["version"]
+    return game_info
