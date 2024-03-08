92d66478529653237f26ddaded4ff7daa3241616
NearlyTRex <evermoresecrets@gmail.com>
2024-03-08 11:24:04 -0500

    Adding optional default value

--- Scripts/lib/system.py
+++ Scripts/lib/system.py
@@ -97,8 +97,11 @@ def AssertPathExists(var_value, var_name):
 ###########################################################
 
 # Prompt for value
-def PromptForValue(description, default_value):
-    value = input(">>> %s [default: %s]: " % (description, default_value))
+def PromptForValue(description, default_value = None):
+    prompt = ">>> %s: " % (description)
+    if default_value:
+        prompt = ">>> %s [default: %s]: " % (description, default_value)
+    value = input(prompt)
     if len(value) == 0:
         return default_value
     return value
