7a5d1a91dd2db0d8b7c42fea8b33b96cd8585591
Pamela Sera <evermoresecrets@gmail.com>
2024-05-02 13:05:57 -0400

    Adding crypt tool

---
+++ Scripts/bin/crypt_tool.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Imports
+import os, os.path
+import sys
+import argparse
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import programs
+import command
+import setup
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Encrypt/decrypt files.")
+parser.add_argument("path", help="Input path")
+parser.add_argument("-p", "--passphrase", type=str, required=True, help="Passphrase for encryption")
+parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
+parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
+args, unknown = parser.parse_known_args()
+if not args.path:
+    parser.print_help()
+    sys.exit(-1)
+
+# Check that path exists first
+root_path = os.path.realpath(args.path)
+if not os.path.exists(root_path):
+    system.LogError("Path '%s' does not exist" % args.path)
+    sys.exit(-1)
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+# Start
+main()
