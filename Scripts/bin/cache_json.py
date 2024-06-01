da373115a55ec306e78a426865bfc3232ab91f8e
Pamela Sera <evermoresecrets@gmail.com>
2024-06-01 12:56:15 -0400

    Missing import

--- Scripts/bin/cache_json.py
+++ Scripts/bin/cache_json.py
@@ -9,6 +9,7 @@ import argparse
 lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
 sys.path.append(lib_folder)
 import config
+import system
 import gameinfo
 import cache
 import setup
