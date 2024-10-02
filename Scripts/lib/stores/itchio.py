0ec7575c893784dbc6a2c9d0cc272a70b03a078a
Pamela Sera <evermoresecrets@gmail.com>
2024-10-01 21:36:06 -0400

    Implementing GetPurchases for itchio

--- Scripts/lib/stores/itchio.py
+++ Scripts/lib/stores/itchio.py
@@ -44,7 +44,7 @@ class Itchio(storebase.StoreBase):
 
     # Get identifier
     def GetIdentifier(self, game_info, identifier_type):
-        return game_info.get_store_appid(self.GetKey())
+        return game_info.get_store_appurl(self.GetKey())
 
     ############################################################
 
@@ -60,7 +60,6 @@ class Itchio(storebase.StoreBase):
         except Exception as e:
             if verbose:
                 system.LogError(e)
-        if not web_driver:
             return False
 
         # Log into itchio
@@ -80,7 +79,7 @@ class Itchio(storebase.StoreBase):
         except Exception as e:
             if verbose:
                 system.LogError(e)
-        return False
+            return False
 
     ############################################################
 
@@ -89,7 +88,72 @@ class Itchio(storebase.StoreBase):
         self,
         verbose = False,
         exit_on_failure = False):
-        return None
+
+        # Create web driver
+        try:
+            web_driver = webpage.CreateWebDriver(verbose = verbose)
+        except Exception as e:
+            if verbose:
+                system.LogError(e)
+            return None
+
+        # Log into itchio
+        success = webpage.LogIntoWebsite(
+            driver = web_driver,
+            login_url = config.itchio_login_url,
+            cookiefile = os.path.join(environment.GetCookieDirectory(), config.itchio_login_cookie_filename),
+            link_text = config.itchio_login_link_text,
+            verbose = verbose)
+        if not success:
+            return False
+
+        # Go to the purchases page
+        try:
+            web_driver.get("https://itch.io/my-purchases")
+        except:
+            return None
+
+        # Scroll to end of page until everything is loaded
+        webpage.ScrollToEndOfPage(web_driver)
+        element_grid_loader = webpage.WaitForPageElement(web_driver, class_name = "grid_loader", verbose = verbose)
+        while element_grid_loader:
+            webpage.ScrollToEndOfPage(web_driver)
+            element_grid_loader = webpage.WaitForPageElement(web_driver, class_name = "grid_loader", verbose = verbose)
+
+        # Parse game cells
+        purchases = []
+        game_cells = webpage.GetElement(web_driver, class_name = "game_cell", all_elements = True)
+        if game_cells:
+            for game_cell in game_cells:
+                game_title = webpage.GetElement(game_cell, class_name = "title", tag_name = "a")
+                game_cover = webpage.GetElement(game_cell, class_name = "lazy_loaded", tag_name = "img")
+
+                # Gather info
+                line_appid = webpage.GetElementAttribute(game_cell, "data-game_id")
+                line_appurl = webpage.GetElementAttribute(game_title, "href")
+                if len(line_appurl.split("/download")) == 2:
+                    line_appurl = line_appurl.split("/download")[0]
+                line_title = webpage.GetElementText(game_title).rstrip(" \n")
+
+                # Create purchase
+                purchase = jsondata.JsonData(
+                    json_data = {},
+                    json_platform = self.GetPlatform())
+                purchase.SetJsonValue(config.json_key_store_appid, line_appid)
+                purchase.SetJsonValue(config.json_key_store_appurl, line_appurl)
+                purchase.SetJsonValue(config.json_key_store_name, line_title)
+                purchases.append(purchase)
+
+        # Destroy web driver
+        try:
+            webpage.DestroyWebDriver(web_driver, verbose = verbose)
+        except Exception as e:
+            if verbose:
+                system.LogError(e)
+            return None
+
+        # Return purchases
+        return purchases
 
     ############################################################
 
