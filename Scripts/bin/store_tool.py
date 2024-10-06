305d836227aa6c1ef4b894d67465ede8deaaba23
Pamela Sera <evermoresecrets@gmail.com>
2024-10-06 00:59:30 -0400

    Adding default for input path based on store

--- Scripts/bin/store_tool.py
+++ Scripts/bin/store_tool.py
@@ -10,6 +10,7 @@ lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib
 sys.path.append(lib_folder)
 import config
 import system
+import environment
 import gameinfo
 import stores
 import setup
@@ -35,23 +36,27 @@ parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose
 parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
 args, unknown = parser.parse_known_args()
 
+# Get store
+store_obj = stores.GetStoreByType(args.store_type)
+if not store_obj:
+    system.LogErrorAndQuit("Invalid store")
+
 # Get input path
 input_path = None
 if system.IsPathValid(args.input_path):
     input_path = os.path.realpath(args.input_path)
     if not os.path.exists(input_path):
         system.LogErrorAndQuit("Path '%s' does not exist" % args.input_path)
+if not input_path:
+    input_path = environment.GetJsonRomMetadataDir(
+        game_category = store_obj.GetCategory(),
+        game_subcategory = store_obj.GetSubcategory())
 
 # Main
 def main():
 
     # Check requirements
     setup.CheckRequirements()
-
-    # Get store
-    store_obj = stores.GetStoreByType(args.store_type)
-    if not store_obj:
-        system.LogErrorAndQuit("Invalid store")
 
     # Load manifest
     if args.load_manifest:
