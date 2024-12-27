168e4f1e8ceb0f1d08480ae99d4f44d7ba1471df
Pamela Sera <evermoresecrets@gmail.com>
2024-12-26 20:56:06 -0500

    Updating backup tool

--- Scripts/bin/backup_tool.py
+++ Scripts/bin/backup_tool.py
@@ -10,34 +10,36 @@ lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib
 sys.path.append(lib_folder)
 import config
 import system
-import environment
-import archive
+import backup
 import setup
 
 # Setup argument parser
 parser = argparse.ArgumentParser(description="Backup tool.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-b", "--backup_type",
-    choices=[
-        config.backup_type_copy,
-        config.backup_type_archive
-    ],
-    default=config.backup_type_copy, help="Backup type"
+    choices=config.BackupType.values(),
+    default=config.BackupType.COPY.value, help="Backup type"
 )
-parser.add_argument("-l", "--source_type",
-    choices=[
-        config.source_type_local,
-        config.source_type_remote
-    ],
-    default=config.source_type_local, help="Source type"
+parser.add_argument("-i", "--input_path", type=str, help="Input path")
+parser.add_argument("-o", "--output_path", type=str, help="Output path")
+parser.add_argument("-u", "--game_supercategory",
+    choices=config.game_supercategories,
+    default=config.game_supercategory_roms,
+    help="Game supercategory"
 )
-parser.add_argument("-u", "--game_supercategory", type=str, help="Game supercategory")
 parser.add_argument("-c", "--game_category", type=str, help="Game category")
 parser.add_argument("-s", "--game_subcategory", type=str, help="Game subcategory")
 parser.add_argument("-n", "--gaming_offset", type=str, help="Game offset")
-parser.add_argument("-d", "--exclude_paths", type=str, default="", help="Excluded paths (comma separated list)")
-parser.add_argument("-o", "--output_base_path", type=str, default=".", help="Output base path")
+parser.add_argument("-l", "--source_type",
+    choices=config.SourceType.values(),
+    default=config.SourceType.LOCAL.value, help="Source type"
+)
+parser.add_argument("-q", "--destination_type",
+    choices=config.SourceType.values(),
+    default=config.SourceType.LOCAL.value, help="Destination type"
+)
+parser.add_argument("-w", "--exclude_paths", type=str, default="", help="Excluded paths (comma separated list)")
 parser.add_argument("-e", "--skip_existing", action="store_true", help="Skip existing files")
-parser.add_argument("-i", "--skip_identical", action="store_true", help="Skip identical files")
+parser.add_argument("-a", "--skip_identical", action="store_true", help="Skip identical files")
 parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
 parser.add_argument("-p", "--pretend_run", action="store_true", help="Do a pretend run with no permanent changes")
 parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable exit on failure mode")
@@ -45,72 +47,60 @@ parser.add_argument("-x", "--exit_on_failure", action="store_true", help="Enable
 # Parse arguments
 args, unknownargs = parser.parse_known_args()
 
-# Get output base path
-output_base_path = os.path.realpath(args.output_base_path)
-if not os.path.exists(output_base_path):
-    system.LogErrorAndQuit("Output base path '%s' does not exist" % args.output_base_path)
-
 # Main
 def main():
 
     # Check requirements
     setup.CheckRequirements()
 
-    # Get input path
-    input_path = environment.GetLockerRootDir(args.source_type)
-    if args.game_supercategory:
-        input_path = os.path.join(input_path, config.locker_type_gaming, args.game_supercategory)
-        if args.game_category:
-            input_path = os.path.join(input_path, args.game_category)
-            if args.game_subcategory:
-                input_path = os.path.join(input_path, args.game_subcategory)
-                if args.game_offset:
-                    input_path = os.path.join(input_path, args.game_offset)
+    # Get source file root
+    source_file_root = backup.ResolvePath(
+        path = args.input_path,
+        source_type = args.source_type,
+        game_supercategory = args.game_supercategory,
+        game_category = args.game_category,
+        game_subcategory = args.game_subcategory,
+        game_offset = args.game_offset)
+    if not system.IsPathDirectory(source_file_root):
+        system.LogErrorAndQuit("Could not resolve source path")
 
-    # Get exclude paths
-    exclude_paths = args.exclude_paths.split(",")
-
-    # Check input path
-    if not os.path.exists(input_path):
-        system.LogErrorAndQuit("Input path '%s' does not exist" % input_path)
+    # Get destination file root
+    dest_file_root = backup.ResolvePath(
+        path = args.output_path,
+        source_type = args.destination_type,
+        game_supercategory = args.game_supercategory,
+        game_category = args.game_category,
+        game_subcategory = args.game_subcategory,
+        game_offset = args.game_offset)
+    if not system.IsPathDirectory(dest_file_root):
+        system.LogErrorAndQuit("Could not resolve destination path")
 
     # Copy files
-    if args.backup_type == config.backup_type_copy:
-        for src_file in system.BuildFileList(input_path, excludes = exclude_paths, use_relative_paths = True):
-            system.SmartCopy(
-                src = os.path.join(input_path, src_file),
-                dest = os.path.join(output_base_path, src_file),
-                show_progress = True,
-                skip_existing = args.skip_existing,
-                skip_identical = args.skip_identical,
-                verbose = args.verbose,
-                pretend_run = args.pretend_run,
-                exit_on_failure = args.exit_on_failure)
+    if args.backup_type == config.BackupType.COPY:
+        backup.CopyFiles(
+            input_base_path = source_file_root,
+            output_base_path = dest_file_root,
+            exclude_paths = args.exclude_paths.split(","),
+            show_progress = True,
+            skip_existing = args.skip_existing,
+            skip_identical = args.skip_identical,
+            verbose = args.verbose,
+            pretend_run = args.pretend_run,
+            exit_on_failure = args.exit_on_failure)
 
     # Archive files
-    elif args.backup_type == config.backup_type_archive:
-        for base_obj in system.GetDirectoryContents(input_path, excludes = exclude_paths):
-            base_dir = os.path.join(input_path, base_obj)
-            if os.path.isdir(base_dir):
-                for sub_obj in system.GetDirectoryContents(base_dir):
-                    sub_dir = os.path.join(base_dir, sub_obj)
-                    sub_file = os.path.join(output_base_path, base_obj, sub_obj + ".7z")
-                    if not os.path.isdir(sub_dir):
-                        continue
-                    if system.DoesPathExist(sub_file, case_sensitive_paths = False, partial_paths = True):
-                        continue
-                    system.MakeDirectory(
-                        dir = system.GetFilenameDirectory(sub_file),
-                        verbose = args.verbose,
-                        pretend_run = args.pretend_run,
-                        exit_on_failure = args.exit_on_failure)
-                    archive.CreateArchiveFromFolder(
-                        archive_file = sub_file,
-                        source_dir = sub_dir,
-                        volume_size = "4092m",
-                        verbose = args.verbose,
-                        pretend_run = args.pretend_run,
-                        exit_on_failure = args.exit_on_failure)
+    elif args.backup_type == config.BackupType.ARCHIVE:
+        backup.ArchiveFiles(
+            input_base_path = source_file_root,
+            output_base_path = dest_file_root,
+            archive_type = config.ArchiveType.SEVENZIP,
+            exclude_paths = args.exclude_paths.split(","),
+            show_progress = True,
+            skip_existing = args.skip_existing,
+            skip_identical = args.skip_identical,
+            verbose = args.verbose,
+            pretend_run = args.pretend_run,
+            exit_on_failure = args.exit_on_failure)
 
 # Start
 main()
---
+++ Scripts/lib/backup.py
@@ -0,0 +1,145 @@
+# Imports
+import os, os.path
+import sys
+
+# Local imports
+import config
+import system
+import environment
+import archive
+
+# Resolve path
+def ResolvePath(
+    path = None,
+    source_type = None,
+    game_supercategory = None,
+    game_category = None,
+    game_subcategory = None,
+    game_offset = None):
+
+    # Use existing path if it exists
+    if system.DoesPathExist(path):
+        return path
+
+    # Start building resolved path
+    resolved_path = environment.GetLockerRootDir(source_type)
+
+    # Augment with gaming categories
+    if game_supercategory:
+        resolved_path = os.path.join(resolved_path, config.LockerType.GAMING.cvalue, game_supercategory)
+        if game_category:
+            resolved_path = os.path.join(resolved_path, game_category)
+            if game_subcategory:
+                resolved_path = os.path.join(resolved_path, game_subcategory)
+                if game_offset:
+                    resolved_path = os.path.join(resolved_path, game_offset)
+
+    # Return result
+    return resolved_path
+
+# Copy files
+def CopyFiles(
+    input_base_path,
+    output_base_path,
+    exclude_paths = [],
+    show_progress = False,
+    skip_existing = False,
+    skip_identical = False,
+    verbose = False,
+    pretend_run = False,
+    exit_on_failure = False):
+
+    # Look at non-excluded dirs in the input base path
+    for src_file in system.BuildFileList(input_base_path, excludes = exclude_paths, use_relative_paths = True):
+
+        # Copy files
+        success = system.SmartCopy(
+            src = os.path.join(input_base_path, src_file),
+            dest = os.path.join(output_base_path, src_file),
+            show_progress = show_progress,
+            skip_existing = skip_existing,
+            skip_identical = skip_identical,
+            verbose = verbose,
+            pretend_run = pretend_run,
+            exit_on_failure = exit_on_failure)
+        if not success:
+            system.LogError("Unable to copy backup files from %s to %s" % (input_base_path, output_base_path))
+            return False
+
+    # Should be successful
+    return True
+
+# Archive files
+def ArchiveFiles(
+    input_base_path,
+    output_base_path,
+    archive_type = None,
+    exclude_paths = [],
+    show_progress = False,
+    skip_existing = False,
+    skip_identical = False,
+    verbose = False,
+    pretend_run = False,
+    exit_on_failure = False):
+
+    # Select archive type
+    if not archive_type:
+        archive_type = config.ArchiveType.SEVENZIP
+
+    # Create temporary directory
+    tmp_dir_success, tmp_dir_result = system.CreateTemporaryDirectory(
+        verbose = verbose,
+        pretend_run = pretend_run)
+    if not tmp_dir_success:
+        return False
+
+    # Look at non-excluded dirs in the input base path
+    for base_obj in system.GetDirectoryContents(input_base_path, excludes = exclude_paths):
+        base_dir = os.path.join(input_base_path, base_obj)
+        if os.path.isdir(base_dir):
+
+            # Only look for subdirectories to archive in each main directory
+            for sub_obj in system.GetDirectoryContents(base_dir):
+                sub_dir = os.path.join(base_dir, sub_obj)
+                if not system.IsPathDirectory(sub_dir):
+                    continue
+
+                # Get paths
+                tmp_archive_file = os.path.join(tmp_dir_result, sub_obj + f".{archive_type.value}")
+                out_archive_file = os.path.join(output_base_path, base_obj, sub_obj + f".{archive_type.value}")
+
+                # Archive subdirectory
+                success = archive.CreateArchiveFromFolder(
+                    archive_file = tmp_archive_file,
+                    source_dir = sub_dir,
+                    volume_size = "4092m",
+                    verbose = verbose,
+                    pretend_run = pretend_run,
+                    exit_on_failure = exit_on_failure)
+                if not success:
+                    system.LogError("Unable to archive backup files from %s to %s" % (input_base_path, output_base_path))
+                    return False
+
+                # Move archive
+                success = system.SmartMove(
+                    src = tmp_archive_file,
+                    dest = out_archive_file,
+                    show_progress = show_progress,
+                    skip_existing = skip_existing,
+                    skip_identical = skip_identical,
+                    verbose = verbose,
+                    pretend_run = pretend_run,
+                    exit_on_failure = exit_on_failure)
+                if not success:
+                    system.LogError("Unable to move archived backup files from %s to %s" % (input_base_path, output_base_path))
+                    return False
+
+    # Delete temporary directory
+    system.RemoveDirectory(
+        dir = tmp_dir_result,
+        verbose = verbose,
+        pretend_run = pretend_run,
+        exit_on_failure = exit_on_failure)
+
+    # Should be successful
+    return True
