2aee246697ab892549d086b8a623b85d4a2fd007
Pamela Sera <evermoresecrets@gmail.com>
2024-12-27 10:15:34 -0500

    Updating rclone configuration file creating

--- Scripts/lib/tools/rclone.py
+++ Scripts/lib/tools/rclone.py
@@ -9,11 +9,22 @@ import environment
 import release
 import programs
 import toolbase
+import ini
 
 # Config files
 config_files = {}
-config_files["RClone/windows/rclone.conf"] = ""
-config_files["RClone/linux/rclone.conf"] = ""
+config_files["RClone/windows/rclone.conf"] = """
+[REMOTE_NAME]
+type = REMOTE_TYPE
+team_drive =
+token = REMOTE_TOKEN
+"""
+config_files["RClone/linux/rclone.conf"] = """
+[REMOTE_NAME]
+type = REMOTE_TYPE
+team_drive =
+token = REMOTE_TOKEN
+"""
 
 # RClone tool
 class RClone(toolbase.ToolBase):
@@ -103,11 +114,20 @@ class RClone(toolbase.ToolBase):
     # Configure
     def Configure(self, verbose = False, pretend_run = False, exit_on_failure = False):
 
+        # Get options
+        locker_remote_name = ini.GetIniValue("UserData.Share", "locker_remote_name")
+        locker_remote_type = ini.GetIniValue("UserData.Share", "locker_remote_type")
+        locker_remote_token = ini.GetIniValue("UserData.Share", "locker_remote_token")
+
         # Create config files
         for config_filename, config_contents in config_files.items():
+            config_contents = config_contents.strip()
+            config_contents = config_contents.replace(config.token_remote_name, locker_remote_name)
+            config_contents = config_contents.replace(config.token_remote_type, locker_remote_type)
+            config_contents = config_contents.replace(config.token_remote_token, locker_remote_token)
             success = system.TouchFile(
                 src = os.path.join(environment.GetToolsRootDir(), config_filename),
-                contents = config_contents.strip(),
+                contents = config_contents,
                 verbose = verbose,
                 pretend_run = pretend_run,
                 exit_on_failure = exit_on_failure)
