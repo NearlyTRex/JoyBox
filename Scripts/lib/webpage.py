3210ea3a65873f5b2b531012145757f5cd3627d7
Pamela Sera <evermoresecrets@gmail.com>
2025-01-20 10:31:17 -0500

    Updating CreateChromeDriver to work better in headless mode

--- Scripts/lib/webpage.py
+++ Scripts/lib/webpage.py
@@ -62,8 +62,15 @@ def CreateChromeWebDriver(
             service = ChromeService(webdriver_tool, log_path=os.path.devnull)
             options = ChromeOptions()
             options.add_argument("--start-maximized")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--disable-extensions")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
             if make_headless:
                 options.add_argument("--headless")
+                options.add_argument("--window-size=1920,1080")
             if system.IsPathValid(binary_location) and system.DoesPathExist(binary_location):
                 options.binary_location = binary_location
             web_driver = Chrome(service=service, options=options)
