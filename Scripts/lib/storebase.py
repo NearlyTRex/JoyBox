927e5f7d2e1f97569e49d49772c0353169a9ea60
Pamela Sera <evermoresecrets@gmail.com>
2025-02-08 12:22:24 -0500

    Moving default GetLatestJsonData to base class

--- Scripts/lib/storebase.py
+++ Scripts/lib/storebase.py
@@ -454,7 +454,47 @@ class StoreBase:
         verbose = False,
         pretend_run = False,
         exit_on_failure = False):
-        return None
+
+        # Check identifier
+        if not self.IsValidIdentifier(identifier):
+            return None
+
+        # Build game info
+        game_info = {}
+        game_info[config.json_key_store_paths] = []
+        game_info[config.json_key_store_keys] = []
+
+        # Augment by manifest
+        if self.manifest:
+            manifest_entry = self.manifest.find_entry_by_name(
+                name = identifier,
+                verbose = verbose,
+                pretend_run = pretend_run,
+                exit_on_failure = exit_on_failure)
+            if manifest_entry:
+
+                # Get existing paths and keys
+                game_paths = set(game_info[config.json_key_store_paths])
+                game_keys = set(game_info[config.json_key_store_keys])
+
+                # Get base path
+                base_path = config.token_game_install_dir
+
+                # Update paths and keys
+                game_paths = list(game_paths.union(manifest_entry.get_paths(base_path)))
+                game_keys = list(game_keys.union(manifest_entry.get_keys()))
+
+                # Remove invalid paths
+                game_paths = [item for item in game_paths if not item.startswith("C:")]
+                game_paths = [item for item in game_paths if not config.token_store_install_dir in item]
+                game_paths = [item for item in game_paths if not config.token_store_user_id in item]
+
+                # Save paths and keys
+                game_info[config.json_key_store_paths] = system.SortStrings(game_paths)
+                game_info[config.json_key_store_keys] = system.SortStrings(game_keys)
+
+        # Return game info
+        return jsondata.JsonData(game_info, self.GetPlatform())
 
     # Update json
     def UpdateJson(
