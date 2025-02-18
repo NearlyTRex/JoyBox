39d0740dff3be1ea75383feb0b382f8a674d3d0a
Pamela Sera <evermoresecrets@gmail.com>
2025-02-17 21:47:43 -0500

    Using new iso module (mounting) for internal functionality
    Adding force overwrite to extract chd

--- Scripts/lib/chd.py
+++ Scripts/lib/chd.py
@@ -10,13 +10,19 @@ import system
 import iso
 import archive
 
+# Get disc iso
+def GetDiscISO(chd_file):
+    return chd_file.replace(config.DiscImageFileType.CHD.cval(), config.DiscImageFileType.ISO.cval())
+
+# Get disc toc
+def GetDiscTOC(chd_file):
+    return chd_file.replace(config.DiscImageFileType.CHD.cval(), config.DiscImageFileType.TOC.cval())
+
 # Check if disc chd is mounted
 def IsDiscCHDMounted(chd_file, mount_dir):
-    return (
-        system.IsPathFile(chd_file) and
-        system.IsPathDirectory(mount_dir) and
-        not system.IsDirectoryEmpty(mount_dir)
-    )
+    return iso.IsISOMounted(
+        iso_file = GetDiscISO(chd_file),
+        mount_dir = mount_dir)
 
 # Create disc chd
 def CreateDiscCHD(
@@ -58,7 +64,7 @@ def CreateDiscCHD(
     # Clean up
     if delete_original:
         system.RemoveFile(
-            file = source_iso,
+            src = source_iso,
             verbose = verbose,
             pretend_run = pretend_run,
             exit_on_failure = exit_on_failure)
@@ -71,6 +77,7 @@ def ExtractDiscCHD(
     chd_file,
     binary_file,
     toc_file,
+    force_overwrite = False,
     delete_original = False,
     verbose = False,
     pretend_run = False,
@@ -92,6 +99,8 @@ def ExtractDiscCHD(
         "-o", toc_file,
         "-ob", binary_file
     ]
+    if force_overwrite:
+        extract_cmd += ["--force"]
 
     # Run extract command
     code = command.RunBlockingCommand(
@@ -108,7 +117,7 @@ def ExtractDiscCHD(
     # Clean up
     if delete_original:
         system.RemoveFile(
-            file = chd_file,
+            src = chd_file,
             verbose = verbose,
             pretend_run = pretend_run,
             exit_on_failure = exit_on_failure)
@@ -155,11 +164,11 @@ def ArchiveDiscCHD(
     # Clean up
     if delete_original:
         system.RemoveFile(
-            file = chd_file,
+            src = chd_file,
             verbose = verbose,
             pretend_run = pretend_run)
     system.RemoveDirectory(
-        dir = tmp_dir_result,
+        src = tmp_dir_result,
         verbose = verbose,
         pretend_run = pretend_run,
         exit_on_failure = exit_on_failure)
@@ -214,52 +223,27 @@ def MountDiscCHD(
     if IsDiscCHDMounted(chd_file, mount_dir):
         return True
 
-    # Create temporary directory
-    tmp_dir_success, tmp_dir_result = system.CreateTemporaryDirectory(
-        verbose = verbose,
-        pretend_run = pretend_run)
-    if not tmp_dir_success:
-        return False
-
-    # Get temporary files
-    temp_iso_file = system.JoinPaths(tmp_dir_result, system.GetFilenameBasename(chd_file) + config.DiscImageFileType.ISO.cval())
-    temp_toc_file = system.JoinPaths(tmp_dir_result, system.GetFilenameBasename(chd_file) + ".toc")
-
     # Extract iso from chd
     success = ExtractDiscCHD(
         chd_file = chd_file,
-        binary_file = temp_iso_file,
-        toc_file = temp_toc_file,
+        binary_file = GetDiscISO(chd_file),
+        toc_file = GetDiscTOC(chd_file),
+        force_overwrite = True,
         verbose = verbose,
         pretend_run = pretend_run,
         exit_on_failure = exit_on_failure)
     if not success:
         return False
 
-    # Make mount directories
-    system.MakeDirectory(
-        dir = mount_dir,
-        verbose = verbose,
-        pretend_run = pretend_run,
-        exit_on_failure = exit_on_failure)
-
-    # Extract iso files to mount point
-    success = archive.ExtractArchive(
-        archive_file = temp_iso_file,
-        extract_dir = mount_dir,
-        delete_original = True,
+    # Mount iso
+    success = iso.MountISO(
+        iso_file = GetDiscISO(chd_file),
+        mount_dir = mount_dir,
         verbose = verbose,
         pretend_run = pretend_run,
         exit_on_failure = exit_on_failure)
     if not success:
         return False
-
-    # Delete temporary directory
-    system.RemoveDirectory(
-        dir = tmp_dir_result,
-        verbose = verbose,
-        pretend_run = pretend_run,
-        exit_on_failure = exit_on_failure)
 
     # Check result
     return IsDiscCHDMounted(chd_file, mount_dir)
@@ -276,12 +260,15 @@ def UnmountDiscCHD(
     if not IsDiscCHDMounted(chd_file, mount_dir):
         return True
 
-    # Remove mount point
-    system.RemoveDirectory(
-        dir = mount_dir,
+    # Unmount iso
+    success = iso.UnmountISO(
+        iso_file = GetDiscISO(chd_file),
+        mount_dir = mount_dir,
         verbose = verbose,
         pretend_run = pretend_run,
         exit_on_failure = exit_on_failure)
+    if not success:
+        return False
 
     # Check result
     return not IsDiscCHDMounted(chd_file, mount_dir)
