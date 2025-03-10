87e69e44cde15a389750f75adbb257bcfe829766
Pamela Sera <evermoresecrets@gmail.com>
2025-03-09 23:41:15 -0400

    Scan rom fixes

--- Scripts/bin/scan_rom_files.py
+++ Scripts/bin/scan_rom_files.py
@@ -10,10 +10,12 @@ sys.path.append(lib_folder)
 import config
 import system
 import environment
-import gameinfo
 import collection
-import metadata
+import gameinfo
 import arguments
+import metadata
+import stores
+import manifest
 import setup
 import ini
 
@@ -44,16 +46,51 @@ def main():
     elif args.passphrase_type == config.PassphraseType.LOCKER:
         passphrase = ini.GetIniValue("UserData.Protection", "locker_passphrase")
 
+    # Load manifest
+    system.LogInfo("Loading manifest ...")
+    store_manifest = manifest.Manifest()
+    store_manifest.load(
+        verbose = args.verbose,
+        pretend_run = args.pretend_run,
+        exit_on_failure = args.exit_on_failure)
+
+    # Import store purchases
+    system.LogInfo("Importing store purchases ...")
+    for store_type in config.StoreType.members():
+        store_obj = stores.GetStoreByType(store_type)
+        if store_obj:
+            if not store_obj.CanImportPurchases():
+                continue
+
+            # Import manifest
+            store_obj.LoadManifest(store_manifest,
+                verbose = args.verbose,
+                pretend_run = args.pretend_run,
+                exit_on_failure = args.exit_on_failure)
+
+            # Login
+            store_obj.Login(
+                verbose = args.verbose,
+                pretend_run = args.pretend_run,
+                exit_on_failure = args.exit_on_failure)
+
+            # Import purchases
+            store_obj.ImportPurchases(
+                verbose = args.verbose,
+                pretend_run = args.pretend_run,
+                exit_on_failure = args.exit_on_failure)
+
     # Build metadata files
     system.LogInfo("Building metadata files ...")
-    for game_supercategory in config.Supercategory.members():
+    for game_supercategory in [config.Supercategory.ROMS]:
         for game_category in config.Category.members():
             for game_subcategory in config.subcategory_map[game_category]:
                 game_platform = gameinfo.DeriveGamePlatformFromCategories(game_category, game_subcategory)
-                game_names = gameinfo.FindJsonGameNames(
+                game_names = gameinfo.FindLockerGameNames(
                     game_supercategory,
                     game_category,
-                    game_subcategory)
+                    game_subcategory,
+                    args.source_type)
                 for game_name in game_names:
 
                     # Get scan path
@@ -79,14 +116,15 @@ def main():
 
     # Build json files
     system.LogInfo("Building json files ...")
-    for game_supercategory in config.Supercategory.members():
+    for game_supercategory in [config.Supercategory.ROMS]:
         for game_category in config.Category.members():
             for game_subcategory in config.subcategory_map[game_category]:
                 game_platform = gameinfo.DeriveGamePlatformFromCategories(game_category, game_subcategory)
-                game_names = gameinfo.FindJsonGameNames(
+                game_names = gameinfo.FindLockerGameNames(
                     game_supercategory,
                     game_category,
-                    game_subcategory)
+                    game_subcategory,
+                    args.source_type)
                 for game_name in game_names:
 
                     # Get scan path
@@ -111,7 +149,7 @@ def main():
 
     # Publish metadata files
     system.LogInfo("Publishing metadata files ...")
-    for game_supercategory in config.Supercategory.members():
+    for game_supercategory in [config.Supercategory.ROMS]:
         for game_category in config.Category.members():
 
             # Publish metadata
