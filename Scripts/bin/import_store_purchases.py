e34f55a6ca2c0c7a937491aebbccb1ec9fa6678a
Pamela Sera <evermoresecrets@gmail.com>
2025-04-19 17:22:38 -0400

    Updating store purchases script

--- Scripts/bin/import_store_purchases.py
+++ Scripts/bin/import_store_purchases.py
@@ -8,13 +8,33 @@ import sys
 lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
 sys.path.append(lib_folder)
 import config
+import environment
 import system
+import gameinfo
 import collection
 import arguments
 import setup
+import ini
 
 # Parse arguments
-parser = arguments.ArgumentParser(description = "Import store purchases.")
+parser = arguments.ArgumentParser(description = "Build store purchases.")
+parser.add_game_supercategory_argument()
+parser.add_game_category_argument()
+parser.add_game_subcategory_argument()
+parser.add_enum_argument(
+    args = ("-l", "--source_type"),
+    arg_type = config.SourceType,
+    default = config.SourceType.REMOTE,
+    description = "Source type")
+parser.add_enum_argument(
+    args = ("-t", "--passphrase_type"),
+    arg_type = config.PassphraseType,
+    description = "Passphrase type")
+parser.add_enum_argument(
+    args = ("-m", "--generation_mode"),
+    arg_type = config.GenerationModeType,
+    default = config.GenerationModeType.STANDARD,
+    description = "Generation mode")
 parser.add_common_arguments()
 args, unknown = parser.parse_known_args()
 
@@ -24,14 +44,57 @@ def main():
     # Check requirements
     setup.CheckRequirements()
 
-    # Import store purchases
-    system.LogInfo("Importing store purchases ...")
-    success = collection.ImportAllStorePurchases(
-        verbose = args.verbose,
-        pretend_run = args.pretend_run,
-        exit_on_failure = args.exit_on_failure)
-    if not success:
-        system.LogError("Importing store purchases failed", quit_program = True)
+    # Get passphrase
+    passphrase = None
+    if args.passphrase_type == config.PassphraseType.GENERAL:
+        passphrase = ini.GetIniValue("UserData.Protection", "general_passphrase")
+    elif args.passphrase_type == config.PassphraseType.LOCKER:
+        passphrase = ini.GetIniValue("UserData.Protection", "locker_passphrase")
+
+    # Manually specify all parameters
+    if args.generation_mode == config.GenerationModeType.CUSTOM:
+        if not args.game_category:
+            system.LogError("Game category is required for custom mode", quit_program = True)
+        if not args.game_subcategory:
+            system.LogError("Game subcategory is required for custom mode", quit_program = True)
+        success = collection.BuildGameStorePurchases(
+            game_supercategory = args.game_supercategory,
+            game_category = args.game_category,
+            game_subcategory = args.game_subcategory,
+            passphrase = passphrase,
+            source_type = args.source_type,
+            verbose = args.verbose,
+            pretend_run = args.pretend_run,
+            exit_on_failure = args.exit_on_failure)
+        if not success:
+            system.LogError(
+                message = "Build of store purchases failed!",
+                game_supercategory = args.game_supercategory,
+                game_category = args.game_category,
+                game_subcategory = args.game_subcategory,
+                quit_program = True)
+
+    # Automatic according to standard layout
+    elif args.generation_mode == config.GenerationModeType.STANDARD:
+        for game_supercategory in parser.get_selected_supercategories():
+            for game_category, game_subcategories in parser.get_selected_subcategories().items():
+                for game_subcategory in game_subcategories:
+                    success = collection.BuildGameStorePurchases(
+                        game_supercategory = game_supercategory,
+                        game_category = game_category,
+                        game_subcategory = game_subcategory,
+                        passphrase = passphrase,
+                        source_type = args.source_type,
+                        verbose = args.verbose,
+                        pretend_run = args.pretend_run,
+                        exit_on_failure = args.exit_on_failure)
+                    if not success:
+                        system.LogError(
+                            message = "Build of store purchases failed!",
+                            game_supercategory = game_supercategory,
+                            game_category = game_category,
+                            game_subcategory = game_subcategory,
+                            quit_program = True)
 
 # Start
 main()
