09da739b53fdb9d0be3665a8f9630d7bf2020203
Pamela Sera <evermoresecrets@gmail.com>
2025-05-10 00:43:51 -0400

    Updating script server

--- Bootstrap/installers/installer_scriptserver.py
+++ Bootstrap/installers/installer_scriptserver.py
@@ -1,12 +1,64 @@
 # Imports
 import os
 import sys
+import re
 
 # Local imports
 import util
 from . import installer
 
-# ScriptServer
+# Nginx config template
+nginx_config_template = """
+server {{
+    listen 80;
+    server_name {subdomain}.{domain};
+
+    location / {{
+        return 301 https://{subdomain}.{domain}$request_uri;
+    }}
+}}
+
+server {{
+    listen 443 ssl;
+    server_name {subdomain}.{domain};
+
+    ssl_certificate /etc/letsencrypt/live/{domain}/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/{domain}/privkey.pem;
+
+    location / {{
+        proxy_pass http://localhost:{port_http};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-Proto https;
+    }}
+}}
+"""
+
+# Docker compose template
+docker_compose_template = """
+version: '3.8'
+services:
+  scriptserver:
+    image: svenstaro/script-server
+    container_name: scriptserver
+    restart: always
+    ports:
+      - "${SCRIPT_SERVER_PORT_HTTP}:5000"
+    volumes:
+      - scriptserver-config:/app/conf
+      - scriptserver-scripts:/app/scripts
+
+volumes:
+  scriptserver-config: {}
+  scriptserver-scripts: {}
+"""
+
+# .env template
+env_template = """
+SCRIPT_SERVER_PORT_HTTP={port_http}
+"""
+
+# ScriptServer Installer
 class ScriptServer(installer.Installer):
     def __init__(
         self,
@@ -15,12 +67,70 @@ class ScriptServer(installer.Installer):
         flags = util.RunFlags(),
         options = util.RunOptions()):
         super().__init__(config, connection, flags, options)
+        self.app_name = "scriptserver"
+        self.app_dir = f"$HOME/apps/{self.app_name}"
+        self.nginx_config_values = {
+            "domain": self.config.GetValue("UserData.Servers", "domain_name"),
+            "subdomain": self.config.GetValue("UserData.ScriptServer", "scriptserver_subdomain"),
+            "port_http": self.config.GetValue("UserData.ScriptServer", "scriptserver_port_http")
+        }
+        self.env_values = {
+            "port_http": self.config.GetValue("UserData.ScriptServer", "scriptserver_port_http")
+        }
 
     def IsInstalled(self):
-        return False
+        containers = self.connection.RunOutput("docker ps -a --format '{{.Names}}'")
+        return any(self.app_name in name for name in containers.splitlines())
 
     def Install(self):
-        return False
+
+        # Create directory
+        util.LogInfo("Creating directory")
+        self.connection.MakeDirectory(self.app_dir)
+
+        # Write docker compose
+        util.LogInfo("Writing docker compose")
+        if self.connection.WriteFile("/tmp/docker-compose.yml", docker_compose_template):
+            self.connection.MoveFileOrDirectory("/tmp/docker-compose.yml", f"{self.app_dir}/docker-compose.yml")
+
+        # Write docker env
+        util.LogInfo("Writing docker env")
+        if self.connection.WriteFile("/tmp/.env", env_template.format(**self.env_values)):
+            self.connection.MoveFileOrDirectory("/tmp/.env", f"{self.app_dir}/.env")
+
+        # Create Nginx entry
+        util.LogInfo("Creating Nginx entry")
+        if self.connection.WriteFile(f"/tmp/{self.app_name}.conf", nginx_config_template.format(**self.nginx_config_values)):
+            self.connection.RunChecked([self.nginx_manager_tool, "install_conf", f"/tmp/{self.app_name}.conf"], sudo=True)
+            self.connection.RunChecked([self.nginx_manager_tool, "link_conf", f"{self.app_name}.conf"], sudo=True)
+            self.connection.RemoveFileOrDirectory(f"/tmp/{self.app_name}.conf")
+
+        # Restart Nginx
+        util.LogInfo("Restarting Nginx")
+        self.connection.RunChecked([self.nginx_manager_tool, "systemctl", "restart"], sudo=True)
+
+        # Start docker
+        util.LogInfo("Starting docker")
+        self.connection.GetOptions().SetCurrentWorkingDirectory(self.app_dir)
+        self.connection.RunChecked([self.docker_compose_tool, "--env-file", f"{self.app_dir}/.env", "up", "-d", "--build"])
+        return True
 
     def Uninstall(self):
-        return False
+
+        # Stop docker
+        util.LogInfo("Stopping docker")
+        self.connection.GetOptions().SetCurrentWorkingDirectory(self.app_dir)
+        self.connection.RunChecked([self.docker_compose_tool, "--env-file", f"{self.app_dir}/.env", "down", "-v"])
+
+        # Remove directory
+        util.LogInfo("Removing directory")
+        self.connection.RemoveFileOrDirectory(self.app_dir)
+
+        # Remove Nginx entry
+        util.LogInfo("Removing Nginx entry")
+        self.connection.RunChecked([self.nginx_manager_tool, "remove_conf", f"{self.app_name}.conf"], sudo=True)
+
+        # Restart Nginx
+        util.LogInfo("Restarting Nginx")
+        self.connection.RunChecked([self.nginx_manager_tool, "systemctl", "restart"], sudo=True)
+        return True
