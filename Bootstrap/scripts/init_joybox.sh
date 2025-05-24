04421d6786792d07731f72576f9edd05b559a02e
Pamela Sera <evermoresecrets@gmail.com>
2025-05-24 11:17:45 -0400

    Adding other init scripts

---
+++ Bootstrap/scripts/init_joybox.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Check common functions
+BASE_DIR="$(dirname "$0")"
+if [[ ! -r "$BASE_DIR/common.sh" ]]; then
+    echo "Error: Cannot find or read $BASE_DIR/common.sh"
+    exit 1
+fi
+
+# Load common functions
+source "$BASE_DIR/common.sh"
+ensure_bash_shell
+ensure_root_user
+
+# Print usage
+print_usage() {
+    echo "Usage: $0 --user USERNAME"
+    exit 1
+}
+
+# Parse arguments
+USERNAME=""
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --user)
+            USERNAME="$2"
+            shift 2
+            ;;
+        -*|--*)
+            echo "Unknown option: $1"
+            print_usage
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
+# Validate required arg
+if [[ -z "$USERNAME" ]]; then
+    echo "Error: --user must be specified."
+    print_usage
+fi
+
+# Check user
+check_user_exists "$USERNAME"
+
+# Setup JoyBox
+clone_joybox_repo "$USERNAME"
