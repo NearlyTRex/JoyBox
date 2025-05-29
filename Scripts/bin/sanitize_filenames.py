72efc2e7e1d653963fa73f1ee36d2fa367684d9a
Pamela Sera <evermoresecrets@gmail.com>
2025-05-28 22:17:21 -0400

    Adding sanitize filenames script

---
+++ Scripts/bin/sanitize_filenames.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Imports
+import os
+import sys
+
+# Custom imports
+lib_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.append(lib_folder)
+import config
+import system
+import arguments
+import setup
+
+# Parse arguments
+parser = arguments.ArgumentParser(description = "Sanitize filenames.")
+parser.add_input_path_argument()
+parser.add_common_arguments()
+args, unknown = parser.parse_known_args()
+
+# Main
+def main():
+
+    # Check requirements
+    setup.CheckRequirements()
+
+    # Get input path
+    input_path = parser.get_input_path()
+
+    # Sanitize filenames
+    system.SanitizeFilenames(
+        path = input_path,
+        verbose = args.verbose,
+        pretend_run = args.pretend_run,
+        exit_on_failure = args.exit_on_failure)
+
+# Start
+main()
