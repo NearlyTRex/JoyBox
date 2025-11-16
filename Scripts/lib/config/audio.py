a8d64bb4caad3a6bc0f8688a5235608027e946e9
Pamela Sera <evermoresecrets@gmail.com>
2025-11-15 22:30:18 -0500

    Updating audio channels

--- Scripts/lib/config/audio.py
+++ Scripts/lib/config/audio.py
@@ -5,20 +5,25 @@ import copy
 
 # Story channels
 story_channels = [
-    {
-        "name": "Unit 522",
-        "url": "https://www.youtube.com/@UNIT522/videos"
-    },
-    {
-        "name": "Mr Nightmare",
-        "url": "https://www.youtube.com/@mrnightmare/videos"
-    }
+    { "name": "Being Scared", "url": "https://www.youtube.com/channel/UCrggft5vtcVpzLVCfQ0FBhg" },
+    { "name": "Blue_Spooky", "url": "https://www.youtube.com/@BlueSpooky/videos" },
+    { "name": "Corpse Husband", "url": "https://www.youtube.com/@CorpseHusband/videos" },
+    { "name": "Doctor Horror", "url": "https://www.youtube.com/@DoctorHorror/videos" },
+    { "name": "Midnight Bard", "url": "https://www.youtube.com/channel/UCNz16DszwHW1HLi5g7-WVpg" },
+    { "name": "Mr Nightmare", "url": "https://www.youtube.com/@mrnightmare/videos" },
+    { "name": "Mr. Haunted", "url": "https://www.youtube.com/channel/UCS_BR42yv9YAx0XFy1tKXZg/videos" },
+    { "name": "Mr. Night Scares", "url": "https://www.youtube.com/@Mr.Nightscares/videos" },
+    { "name": "Mr.Spooky", "url": "https://www.youtube.com/@MrSpookyStories/videos" },
+    { "name": "Night Time Spooks", "url": "https://www.youtube.com/@nighttimespooks/videos" },
+    { "name": "Ripshy", "url": "https://www.youtube.com/@heyshyily" },
+    { "name": "Scarystuff Ding", "url": "https://www.youtube.com/@scarystuffscarystuffscarys3780/videos" },
+    { "name": "Southern Cannibal", "url": "https://www.youtube.com/@SouthernCannibal/videos" },
+    { "name": "Stop and Scare!", "url": "https://www.youtube.com/@StopandScare-/videos" },
+    { "name": "Unit 522", "url": "https://www.youtube.com/@UNIT522/videos" },
+    { "name": "Whispered Diaries", "url": "https://www.youtube.com/@whispereddiaries/videos" }
 ]
 
 # ASMR channels
 asmr_channels = [
-    {
-        "name": "Gentle Whispering ASMR",
-        "url": "https://www.youtube.com/@GentleWhisperingASMR/videos"
-    }
+    { "name": "Gentle Whispering ASMR", "url": "https://www.youtube.com/@GentleWhisperingASMR/videos" }
 ]
