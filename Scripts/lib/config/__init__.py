bc47cd68eda5bc989a8e64c7c7a1e2e21ec781bb
Pamela Sera <evermoresecrets@gmail.com>
2025-12-06 09:57:40 -0500

    Adding decompiler config

--- Scripts/lib/config/__init__.py
+++ Scripts/lib/config/__init__.py
@@ -1,6 +1,7 @@
 # Imports
 from config.audio import *
 from config.categories import *
+from config.decompiler import *
 from config.files import *
 from config.general import *
 from config.keys import *
---
+++ Scripts/lib/config/decompiler.py
@@ -0,0 +1,30 @@
+# Imports
+import os
+import sys
+
+# Decompiler presets
+decompiler_presets = {
+    "NocturneDecomp": {
+        "description": "Nocturne game decompilation project",
+        "repo_path": "~/Repositories/NocturneDecomp",
+        "project_dir": "projects",
+        "project_name": "NocturneEdit",
+        "program_name": "nocedit.exe",
+        "project_language": "x86:LE:32:watcom",
+        "project_cspec": "watcomcpp",
+        "scripts": {
+            "export_annotations": {
+                "description": "Export all annotations to JSON files",
+                "script_path": "scripts/Jython",
+                "script_name": "export_annotations_headless.py",
+                "default_args": ["annotations/nocedit.exe"]
+            },
+            "import_annotations": {
+                "description": "Import annotations from JSON files",
+                "script_path": "scripts/Jython",
+                "script_name": "import_annotations_headless.py",
+                "default_args": ["annotations/nocedit.exe"]
+            }
+        }
+    }
+}
